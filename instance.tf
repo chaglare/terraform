9b3fbe70b8eb478f97c82970c062018d090af2ef
Caglar Cavdar <chaglare@gmail.com>
2019-07-28 16:52:32 -0500

    tags added

--- instance.tf
+++ instance.tf
@@ -2,4 +2,12 @@ resource "aws_instance" "web1" {
     ami = "ami-0b898040803850657"
     instance_type = "t2.micro"
     key_name = "terraform_april"
+    
+    
+    tags = {
+        Name = "Dev"
+        Dept = "IT"
+        Group = "April"
+        Created_by = "Caglar"
+  }
 }
