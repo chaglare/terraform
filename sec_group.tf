a8de0d3cb97a524e6d135525b6122baa59cb02b0
Caglar Cavdar <chaglare@gmail.com>
2019-07-28 16:41:33 -0500

    egress added

--- sec_group.tf
+++ sec_group.tf
@@ -8,5 +8,10 @@ resource "aws_security_group" "sec1" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }  
+  }
+   egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]  
 }
