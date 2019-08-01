3f5278f2913ce0aea324c22ba579708a4db92412
Caglar Cavdar <chaglare@gmail.com>
2019-07-31 19:04:38 -0500

    bucket output

--- output.tf
+++ output.tf
@@ -1,3 +1,8 @@
 output "key_name" {
-    value = "{$aws_key_pair.terrafrom_april.key_name}"
-    }
+    value = "${aws_key_pair.terrafrom_april.key_name}"
+}
+
+
+output "bucketname" {
+    value = "${aws_s3_bucket.b.bucket}"
+}
