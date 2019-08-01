62ae032c40488cf91b944453a0c1ac6f6a81fc6d
Caglar Cavdar <chaglare@gmail.com>
2019-07-31 20:37:12 -0500

    dynamodb commanded

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3"{
-        dynamodb_table = "dev_state_lock"
+        #dynamodb_table = "dev_state_lock"
         bucket = "caglar-tf-bucket"
         region = "us-east-1"
         key = "dev.tfstate"
